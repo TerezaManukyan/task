e13388a5f2ed5d0229bc02934d65e1fc5f6d28be
TerezaManukyan <79563596+TerezaManukyan@users.noreply.github.com>
2023-12-04 12:55:39 +0400

    fixed word

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-<h2>Welcome to my syte</h2>
+<h2>Welcome to my site</h2>
 <a href="{{ route('loginForm') }}">Go to Login page</a>
 </body>
 </html>
